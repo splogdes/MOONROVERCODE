b431660cf3859ad1997e005067fa61e77c6363d5
splogdes <95136830+splogdes@users.noreply.github.com>
2022-06-19 02:30:02 +0100

    Create java.js

---
+++ Website/java.js
@@ -0,0 +1,35 @@
+var last_key = 0;
+function changecolor(i){
+    document.getElementById(i).style.backgroundColor = "rgb(93, 93, 93)";
+};
+document.onkeydown = function down(event){
+    var key_press = event.keyCode;
+    if(key_press == last_key){
+    }else if(key_press == 87){
+        Fforwards();changecolor("f");
+    }else if(key_press == 83){
+        Fbackwards();changecolor("b");
+    }else if(key_press == 65){
+        Fleft();changecolor("l");
+    }else if(key_press == 68){
+        Fright();changecolor("r");
+    }else if(key_press == 81){
+        scan();changecolor("s");
+    }else if(key_press == 38){
+        Sforwards();changecolor("f");
+    }else if(key_press == 40){
+        Sbackwards();changecolor("b");
+    }else if(key_press == 37){
+        Sleft();changecolor("l");
+    }else if(key_press == 39){
+        Sright();changecolor("r");
+    }
+    last_key = key_press;
+};
+document.onkeyup = function up(event){
+    neutral();
+    for (let i = 0; i < 5; i++){
+        document.getElementsByClassName("btn")[i].style.backgroundColor = "rgb(54, 54, 54)";
+    }
+    last_key = 0;
+};
