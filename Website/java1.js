7c5723b6246057f1c6a1fd5f0e2164758cbbd78d
splogdes <95136830+splogdes@users.noreply.github.com>
2022-06-20 13:08:53 +0100

    Update java1.js

--- Website/java1.js
+++ Website/java1.js
@@ -20,3 +20,4 @@ function Sforwards(){xhttp.open("GET", "/sf"); xhttp.send();}
 function Sbackwards(){xhttp.open("GET", "/sb"); xhttp.send();}
 function neutral(){xhttp.open("GET", "/nu"); xhttp.send();}
 function scan(){miner.open("GET", "/s");miner.send();}
+//sets up the URI based on the function
