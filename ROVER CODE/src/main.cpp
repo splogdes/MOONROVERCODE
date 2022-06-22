f77c7d65c398d96f98e9512623a5c0d8ac45b7b0
splogdes <95136830+splogdes@users.noreply.github.com>
2022-06-22 18:41:20 +0100

    Update main.cpp

--- ROVER CODE/src/main.cpp
+++ ROVER CODE/src/main.cpp
@@ -2,8 +2,8 @@
 #define USE_WIFI101           true
 #include <WiFiWebServer.h>
 
-const char ssid[] = "iPhone";
-const char pass[] = "12345678";
+const char ssid[] = "EEERover";
+const char pass[] = "exhibition";
 
 //Webpage to return when root is requested
 const char webpage[] = \
